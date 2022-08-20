a8f505897cb7ca05aa77e20c516ae61fbf347288
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-20 12:13:11 +0600

    map added

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_addressbook_app/models/contact_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   static const String routeName = '/contact_details';
@@ -9,8 +13,90 @@ class ContactDetailsPage extends StatefulWidget {
 }
 
 class _ContactDetailsPageState extends State<ContactDetailsPage> {
+  late ContactModel contact;
+
+  @override
+  void didChangeDependencies() {
+    contact = ModalRoute.of(context)!.settings.arguments as ContactModel; 
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(contact.name),
+      ),
+      body: ListView(
+        children: [
+          contact.image == null ?
+              Image.asset('images/person.png',width: double.maxFinite,height: 250,fit: BoxFit.cover,):
+              Image.file(File(contact.image!),width: double.maxFinite,height: 250,fit: BoxFit.cover,),
+          ListTile(
+            title: Text(contact.mobile),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                    onPressed: _callPerson,
+                    icon: Icon(Icons.call,color: Colors.green,),
+                ),
+                IconButton(
+                  onPressed: _smsContact,
+                  icon: Icon(Icons.sms,color: Colors.green,),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text(contact.email == null || contact.email!.isEmpty ? 'Unavailable' : contact.email!),
+            trailing: IconButton(
+              icon: Icon(contact.email == null || contact.email!.isEmpty ? Icons.edit : Icons.email,color: Colors.green,),
+              onPressed: _mailContact,
+            ),
+          ),
+          ListTile(
+            title: Text(contact.streetAddress == null || contact.streetAddress!.isEmpty ? 'Unavailable' : contact.streetAddress!),
+            trailing: IconButton(
+              icon: Icon(contact.streetAddress == null || contact.streetAddress!.isEmpty ? Icons.edit :Icons.map,color: Colors.green,),
+              onPressed: _showMap,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _callPerson() async{
+    final uri = Uri.parse('tel:${contact.mobile}');
+    if(await canLaunchUrl(uri)){
+     await launchUrl(uri);
+    }else{
+      throw 'cannot launch url';
+    }
+  }
+
+  void _smsContact() {
+
+  }
+
+  void _mailContact() {
+
+  }
+
+  void _showMap() async{
+    String urlString;
+    if(Platform.isAndroid){
+      urlString = 'geo:0,0?q=${contact.streetAddress}';
+    }else if(Platform.isIOS){
+      urlString = 'http://maps.apple.com/?q=${contact.streetAddress}';
+    }else{
+      urlString = 'geo:0,0?q=${contact.streetAddress}';
+    }
+    if(await canLaunchUrl(Uri.parse(urlString))){
+      await launchUrl(Uri.parse(urlString));
+    }else{
+    throw 'cannot launch url';
+    }
   }
 }
